ba2acf22c5aa9be18012f47750d7a429319223df
mo2men165 <moamenabdeltawab@gmail.com>
2024-07-27 12:28:34 +0300

    sentry-fix

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  }), sentryVitePlugin({
     org: "momen-abdeltawab",
     project: "javascript-react"
   })],
